1d6491d73fba5683dba207e5022e6d15c2043ee7
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-09-26 12:17:53 -0500

    Corrección errores

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -5,7 +5,7 @@ class Reptil(Animal):
     serpientes = 0
     def __init__(self, nombre, edad, habitat, genero, colorEscamas, largoCola):
         super().__init__(nombre, edad, habitat, genero)
-        self._colorEscamas = colorEscama
+        self._colorEscamas = colorEscamas
         self._largoCola = largoCola
         Reptil._listaReptil.append(self)
     @classmethod
